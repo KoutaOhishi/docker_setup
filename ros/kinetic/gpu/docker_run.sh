594ce457110ebbac4df6e7a936b69902421831bc
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-01 19:21:30 +0900

    add shell file to run docker image

---
+++ ros/kinetic/gpu/docker_run.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+docker run -p 6080:80 \
+     --device /dev/dri:/dev/dri \
+     --device /dev/video0:/dev/video0:mwr \
+     --shm-size=512m \
+     --name kinetic_gpu #container_name\
+     --previleged \
+     kinetic_gpu #image_name
