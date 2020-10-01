8f35f052212e42a5a3be8aacb01bc075d16ab452
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-01 19:26:29 +0900

    fix typo

--- ros/kinetic/gpu/docker_run.sh
+++ ros/kinetic/gpu/docker_run.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 docker run -p 6080:80 \
-     ---gpus all \
+     --gpus all \
      --device /dev/dri:/dev/dri \
      --device /dev/video0:/dev/video0:mwr \
      --shm-size=512m \
-     --name kinetic_gpu #container_name\
+     --name kinetic_gpu \
      --privileged \
-     kinetic_gpu #image_name
+     kinetic_gpu 
