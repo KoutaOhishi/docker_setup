f21796ff6468e977f3548e1430f400223eaaea14
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-02 17:04:00 +0900

    fix shell file

--- ros/kinetic/gpu/docker_run.sh
+++ ros/kinetic/gpu/docker_run.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
-docker run -p 6080:80 \
+docker run \
+     -p 6080:80 \
+     -p 80:80 \
      --gpus all \
      --device /dev/dri:/dev/dri \
      --device /dev/video0:/dev/video0:mwr \
      --shm-size=512m \
      --name kinetic_gpu \
      --privileged \
-     kinetic_gpu 
+     kinetic_gpu
